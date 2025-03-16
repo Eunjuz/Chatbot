3d229c2a6c2676a68a1bffb6290535b5d5375787
Justin <Justin@J-GUIAB>
2025-03-17 04:26:31 +0800

    Updated frontend + homepage + hardcoded chatbot reply

---
+++ app/src/main/java/com/example/chatbot/HomepageActivity.kt
@@ -0,0 +1,15 @@
+package com.example.chatbot
+import android.content.Intent
+import android.os.Bundle
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+
+class HomepageActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_homepage)
+
+
+    }
+}
